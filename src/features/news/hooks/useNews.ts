3762349419a8ba4535b457755a3259577051babd
Sebastian Ferrari <sebaferrari23@gmail.com>
2026-05-22 16:41:11 -0300

    refactor: add newsReponse type and mapper function

--- src/features/news/hooks/useNews.ts
+++ src/features/news/hooks/useNews.ts
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { apiClient } from '@/api/client';
-import { News } from '../types/news.types';
+import { mapResponseToNews, News, NewsResponse } from '../types/news.types';
 
 export function useNews() {
   return useQuery({
     queryKey: ['news'],
     queryFn: async (): Promise<News[]> => {
-      return await apiClient<News[]>('/posts');
+      const news = await apiClient<NewsResponse[]>('/posts');
+      return news.map(mapResponseToNews);
     },
   });
 }
--- src/features/news/types/news.types.ts
+++ src/features/news/types/news.types.ts
@@ -1,4 +1,4 @@
-export interface News {
+export interface NewsResponse {
   id: number;
   userId: number;
   title: string;
@@ -11,4 +11,24 @@ export interface News {
   publishedAt: string;
   updatedAt: string;
   url: string;
+}
+
+export interface News {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+  thumbnail: string;
+  url: string;
+}
+
+export function mapResponseToNews(response: NewsResponse): News {
+  return {
+    id: response.id,
+    title: response.title,
+    content: response.content,
+    image: response.image,
+    thumbnail: response.thumbnail,
+    url: response.url,
+  };
 }
